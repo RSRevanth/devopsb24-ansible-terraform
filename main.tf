d829b71d9a7c9726ced1f0d75201cef662104148
satya <satya@example.com>
2023-07-12 10:34:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "rayudu1terraformbucket"
-    key    = "ansible.tfstate"
+    key    = "ansible1.tfstate"
     region = "us-east-1"
   }
 }
